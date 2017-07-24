1f939751c1d8b4553d79881bf59f04fd072ad4a3
Jonas Bernoulli <jonas@bernoul.li>
2017-07-24 15:34:00 +0200

    [WIP] magit-collab.el: new library
    That also brings in a new dependency, `ghub', which still needs a bit
    of work before it can go mainstream.

--- default.mk
+++ default.mk
@@ -64,6 +64,7 @@ ELS += magit.el
 ELS += magit-status.el
 ELS += magit-refs.el
 ELS += magit-files.el
+ELS += magit-collab.el
 ELS += magit-branch.el
 ELS += magit-worktree.el
 ELS += magit-notes.el
@@ -98,12 +99,14 @@ VERSION := $(shell \
 MAGIT_VERSION       = 2.10
 ASYNC_VERSION       = 1.9
 DASH_VERSION        = 2.13.0
+GHUB_VERSION        = 1.3     # TODO release me
 WITH_EDITOR_VERSION = 2.5.10
 GIT_COMMIT_VERSION  = 2.10.2
 MAGIT_POPUP_VERSION = 2.10.2
 
 ASYNC_MELPA_SNAPSHOT       = 20170219.942
 DASH_MELPA_SNAPSHOT        = 20170207.2056
+GHUB_SNAPSHOT              = 2017            # TODO
 WITH_EDITOR_MELPA_SNAPSHOT = 20170111.609
 GIT_COMMIT_MELPA_SNAPSHOT  = 20170214.347
 MAGIT_POPUP_MELPA_SNAPSHOT = 20170214.347
@@ -129,6 +132,13 @@ ifeq "$(DASH_DIR)" ""
   DASH_DIR = $(TOP)../dash
 endif
 
+GHUB_DIR ?= $(shell \
+  find -L $(ELPA_DIR) -maxdepth 1 -regex '.*/ghub-[.0-9]*' 2> /dev/null | \
+  sort | tail -n 1)
+ifeq "$(GHUB_DIR)" ""
+  GHUB_DIR = $(TOP)../ghub
+endif
+
 WITH_EDITOR_DIR ?= $(shell \
   find -L $(ELPA_DIR) -maxdepth 1 -regex '.*/with-editor-[.0-9]*' 2> /dev/null | \
   sort | tail -n 1)
@@ -145,9 +155,11 @@ LOAD_PATH = -L $(TOP)/lisp
 
 ifdef CYGPATH
   LOAD_PATH += -L $(shell cygpath --mixed $(DASH_DIR))
+  LOAD_PATH += -L $(shell cygpath --mixed $(GHUB_DIR))
   LOAD_PATH += -L $(shell cygpath --mixed $(WITH_EDITOR_DIR))
 else
   LOAD_PATH += -L $(DASH_DIR)
+  LOAD_PATH += -L $(GHUB_DIR)
   LOAD_PATH += -L $(WITH_EDITOR_DIR)
 endif
 
